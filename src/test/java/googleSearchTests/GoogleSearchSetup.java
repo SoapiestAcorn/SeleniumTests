5893c21f7893f2b914a74055388b81d926d8e74c
SoapiestAcorn <youngal72@gmail.com>
2023-07-26 16:20:26 -0600

    no

--- src/test/java/googleSearchTests/GoogleSearchSetup.java
+++ src/test/java/googleSearchTests/GoogleSearchSetup.java
@@ -1,25 +1,59 @@
 package googleSearchTests;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.ExtentReports;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+
 public class GoogleSearchSetup {
-    public static WebDriver driver;
+
+    protected static WebDriver driver;
+    protected static ExtentReports extent;
+    protected static ExtentTest test;
 
     @BeforeSuite
-    public static void setup() {
+    public void setupReport() {
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter("ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+    }
+
+    @BeforeMethod
+    public void testSetup(Method method) {
+        test = extent.createTest(method.getName(), "Test Description");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-    @AfterSuite
-    public static void teardown() {
+
+    @AfterMethod
+    public void afterTest(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, "Test Failed: " + result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            test.log(Status.SKIP, "Test Skipped: " + result.getThrowable());
+        } else {
+            test.log(Status.PASS, "Test Passed");
+        }
+
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    @AfterSuite
+    public void tearDownReport() {
+        extent.flush();
     }
 }
--- src/test/java/googleSearchTests/GoogleSearchTests.java
+++ src/test/java/googleSearchTests/GoogleSearchTests.java
@@ -1,59 +1,60 @@
 package googleSearchTests;
 
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-import static googleSearchTests.GoogleSearchSetup.driver;
 
-public class GoogleSearchTests {
+public class GoogleSearchTests extends GoogleSearchSetup {
 
-    @Test(priority = 1)
+    @Test(priority = 1,
+            description = "Validate that Google searching Amazon and clicking Amazon link should redirect user to amazon.ca ")
+
     public void testGoogleSearchForAmazon() {
 
         // Go to Google.ca
         driver.get("https://www.google.ca");
-        Reporter.log("Google.ca opened");
+        test.log(Status.INFO, "Step 1: Go to google.ca");
 
         // Enter Amazon in search box
         WebElement searchBox = driver.findElement(By.className("gLFyf"));
         searchBox.sendKeys("Amazon");
         searchBox.submit();
-        Reporter.log("Amazon searched");
+        test.log(Status.INFO, "Step 2: Google search for Amazon");
 
         // Click on Amazon link
         WebElement amazonLink = driver.findElement(By.className("LC20lb"));
         amazonLink.click();
-        Reporter.log("Amazon link clicked");
+        test.log(Status.INFO, "Step 3: Click on Amazon link");
 
         //Expected Result: User should be on amazon.ca
         String currentUrl = driver.getCurrentUrl();
         Assert.assertTrue(currentUrl.contains("amazon.ca"), "Failed - User is not on amazon.ca");
-        Reporter.log("Passed - User is on amazon.ca");
-
+        test.log(Status.PASS, "Expected Result: User is on amazon.ca - PASS");
     }
 
     @Test(priority = 2)
     public void testGoogleSearchForFacebook() {
 
-        // Go to Google.ca
         driver.get("https://www.google.ca");
+        test.log(Status.INFO, "Step 1: Go to google.ca");
 
-        // Enter Facebook in search box
         WebElement searchBox = driver.findElement(By.className("gLFyf"));
         searchBox.sendKeys("Facebook");
         searchBox.submit();
+        test.log(Status.INFO, "Step 2: Google search for Facebook");
 
         // Click on Facebook link
         WebElement facebookLink = driver.findElement(By.className("LC20lb"));
         facebookLink.click();
+        test.log(Status.INFO, "Step 3: Click on Facebook link");
 
         // Expected Result: User should be on facebook.com
         String currentUrl = driver.getCurrentUrl();
         Assert.assertTrue(currentUrl.contains("facebook.com"), "Failed - User is not on facebook.com");
-
+        test.log(Status.PASS, "Expected Result: User is on facebook.com - PASS");
     }
 
 }
